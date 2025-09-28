0d33327b4e5fe6cad7fd657acb958a567596c05e
smaran7577 <saismaran6219@gmail.com>
2025-09-28 21:46:50 +0000

    mod-4

---
+++ loops.js
@@ -0,0 +1,27 @@
+//
+let n=10
+
+while(n<=10 && n>0){
+    console.log(n);
+    n--;
+}
+
+
+for(let i=1;i<10;i+=2){
+    console.log(i);
+}
+console.log("new.  ");
+let b=[1,2,3,4,5,6];
+for(nums of b){
+    console.log(nums);
+}
+
+    let a = [[1, 2, 3], [4, 5, 6]];
+    for (let i = 0; i < a.length; i++) {
+        console.log("list");
+        for (let j = 0; j < a[i].length; j++) {
+            console.log(a[i][j]);
+
+        }
+    }
+
