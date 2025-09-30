cd174c2727670e7a10a347bb51ca6065b6a49555
smaran7577 <saismaran6219@gmail.com>
2025-09-30 09:13:58 +0000

    basics -2

---
+++ basics-level-2/functions.js
@@ -0,0 +1,66 @@
+/*functions are block of code that is used to do 
+some action in our code basically reduces the re writing of the code
+also scope is same in java  */
+
+//function defination
+
+function print1to5 (){
+    for(let i=1;i<=5;i++){
+    console.log(i);
+    }
+}
+
+//function calling 
+console.log(print1to5());
+
+//function with arguments
+
+function info(className, section) {
+  console.log(`Smaran's ${className} is section ${section}`);
+}
+
+info("IT", "B");
+//multiplication table 
+
+function table(num){
+    for(let i=num;i<=num*10;i=i+num){
+        console.log(i);
+    }
+}
+console.log(table(5));
+
+//return 
+function sumofn(n){
+    let sum=(n*(n+1))/2;
+    return sum;
+}
+console.log(sumofn(10));
+//scope
+let greet = "hello";
+
+        function changeGreet () {
+                    let greet = "namaste";
+                console. log (greet) ;
+              function innerGreet() {
+                     console. log (greet);
+              }
+            }
+           console. log (greet);
+      changeGreet ();
+      //function expressions
+      let hello = function (){
+        console.log(hello);
+      }
+      //higher order function 
+       function multiplegreet(func,cn){
+        for(let i=0;i<cn;i++){
+            func();
+        }
+       }
+
+
+
+      let greet1=function () {
+    console.log("hello");
+      }
+    console.log(multiplegreet(greet1,2));
---
+++ basics-level-2/objliterals.js
@@ -0,0 +1,54 @@
+//object literals are collection of properties 
+//like (key,value)
+const student ={
+    rollno : "A9",
+    class : "it-b",
+    cgpa : [9.2,"1st sem"],
+
+
+}
+
+//to access inside values 
+//syntax is student["rollno"] or student.rollno;
+//the key value ie roll no and class cgpa are first converted into strings and then matched to 
+//values 
+console.log(student.rollno);
+
+//to modify values in objlit
+student.cgpa = "9.5";
+
+console.log(student);
+
+//to delete 
+delete student.rollno;
+console.log(student);
+
+//object of objects
+const students={
+         smaran :{
+            rollno:"a9",
+            city:"Hyd",
+         },
+         abhinav : {
+            rollno : "xyz",
+            city : "bglr",
+         }
+}
+console.log(students);
+
+console.log(students.smaran.rollno);
+
+//array of objects
+
+const stud =[
+{
+    rollno: "a9", 
+    class : "it-b",
+},
+{
+    rollno:"xyz",city : "bglr",
+}
+
+
+]
+console.log(stud[0].class);
